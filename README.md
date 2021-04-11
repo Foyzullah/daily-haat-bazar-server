5dd3199d549c990d110e36ff7d8912e46b61f6c5
Foyzullah <foyz.cse@gmail.com>
2021-04-11 21:30:10 +0600

    daily-haat-bazar-server

---
+++ README.md
@@ -0,0 +1,13 @@
+# Daily-Haat-Bazar
+Daily-Haat-Bazar is a simple fake API for testing and prototyping
+
+# Features
+- No registration
+- Zero Config
+- Basic API
+- Get all products and filtered product
+- You can show order list
+- Manage products
+- Can GET,POST,DELETE
+- Cross-domain
+- Others
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q7rxz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const port = process.env.PORT || 5055;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("I am Working");
+});
+
+client.connect((err) => {
+  // Products collection
+  const productsCollection = client.db("dailyHaatBazar").collection("products");
+
+  // Order Collection
+  const ordereCollection = client
+    .db("dailyHaatBazar")
+    .collection("orderedProducts");
+
+  // Add Products
+  app.post("/addProduct", (req, res) => {
+    const newProduct = req.body;
+    productsCollection.insertOne(newProduct).then((result) => {
+      console.log("InsertCount", result.insertedCount);
+      res.send(result);
+    });
+  });
+
+  // Delete Product
+  app.delete("/deleteProduct/:id", (req, res) => {
+    const id = ObjectId(req.params.id);
+    productsCollection.findOneAndDelete({ _id: id });
+  });
+
+  // Get All Products
+  app.get("/allProducts", (req, res) => {
+    productsCollection.find().toArray((error, products) => {
+      res.send(products);
+    });
+  });
+
+  // Get Product by id
+  app.get("/product/:id", (req, res) => {
+    productsCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, product) => {
+        res.send(product[0]);
+      });
+  });
+
+  // Place Order
+  app.post("/addOrder", (req, res) => {
+    const newOrder = req.body;
+    ordereCollection.insertOne(newOrder).then((result) => {
+      res.send(result);
+    });
+  });
+
+  // Get filter orders
+  app.get("/orders", (req, res) => {
+    ordereCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Daily Haat-Bazar app listening at http://localhost:${port}`);
+});
